259e925d91bad988eca601d766faf7b89ca92232
Paul Millar <paul.millar@desy.de>
2014-11-12 19:56:04 +0100

    Add more tests for CellAdmin.
    NB.  The assumeThat causes the test to always succeed for HtmlUnitDriver.
    This is not the intended behaviour; we want the test to be ignored.

--- src/main/java/org/dcache/webtests/webadmin/pages/CellAdminPage.java
+++ src/main/java/org/dcache/webtests/webadmin/pages/CellAdminPage.java
@@ -1,6 +1,24 @@
 package org.dcache.webtests.webadmin.pages;
 
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.Matchers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.refreshed;
+import static org.junit.Assume.*;
 
 /**
  *
@@ -16,5 +34,52 @@ public class CellAdminPage extends DcachePage
     public static boolean isCurrentPage(WebDriver driver)
     {
         return driver.getTitle().equals("Cell Admin");
+    }
+
+    private Select getDomainSelect()
+    {
+        return new Select(_driver.findElement(By.name("cellAdminDomain")));
+    }
+
+    private Select getCellSelect()
+    {
+        return new Select(_driver.findElement(By.name("cellAdminCell")));
+    }
+
+    public List<String> getDomains()
+    {
+        List<String> domains = new ArrayList<>();
+
+        for (WebElement option: getDomainSelect().getOptions()) {
+            domains.add(option.getText());
+        }
+
+        return domains;
+    }
+
+    public List<String> getCells()
+    {
+        List<String> cells = new ArrayList<>();
+
+        for (WebElement option: getCellSelect().getOptions()) {
+            cells.add(option.getText());
+        }
+
+        return cells;
+    }
+
+    public void setDomain(String domain)
+    {
+        assumeThat("Cannot run this test with htmlunit",
+                _driver, not(instanceOf(HtmlUnitDriver.class)));
+
+        Select select = new Select(_driver.findElement(By.name("cellAdminDomain")));
+        select.selectByVisibleText(domain);
+
+        // Updating cellAdminDomain triggers an AJAX event that updates the
+        // list of available cells.  We must wait for this to complete to
+        // avoid race conditions.
+        WebDriverWait wait = new WebDriverWait(_driver, 10);
+        wait.until(refreshed(presenceOfElementLocated(By.name("cellAdminCell"))));
     }
 }
--- src/main/java/org/dcache/webtests/webadmin/tests/CellAdminTests.java
+++ src/main/java/org/dcache/webtests/webadmin/tests/CellAdminTests.java
@@ -2,8 +2,14 @@ package org.dcache.webtests.webadmin.tests;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import org.dcache.webtests.webadmin.pages.CellAdminPage;
 import org.dcache.webtests.webadmin.pages.LoginPage;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -25,5 +31,39 @@ public class CellAdminTests extends AbstractWebDriverTests
                 assertPageIs(CellAdminPage.class);
 
         page.assertLoggedIn();
+    }
+
+    @Test
+    public void testDomainsContainsDcacheDomain()
+    {
+        CellAdminPage page = overview.login().navigateToCellAdmin().
+                assertPageIs(CellAdminPage.class);
+
+        List<String> domains = page.getDomains();
+
+        assertThat(domains, hasItem("dCacheDomain"));
+    }
+
+    @Test
+    public void testCellsInitialEmpty()
+    {
+        CellAdminPage page = overview.login().navigateToCellAdmin().
+                assertPageIs(CellAdminPage.class);
+
+        List<String> cells = page.getCells();
+
+        assertThat(cells, contains("Choose One"));
+    }
+
+    @Test
+    public void testCellsWhenDcacheDomainSelected()
+    {
+        CellAdminPage page = overview.login().navigateToCellAdmin().
+                assertPageIs(CellAdminPage.class);
+
+        page.setDomain("dCacheDomain");
+
+        // FIXME: we're embedding knowledge that should be abstracted out.
+        assertThat(page.getCells(), hasItem("LoginBroker"));
     }
 }
